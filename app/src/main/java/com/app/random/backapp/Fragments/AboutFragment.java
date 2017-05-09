dbf8f394a547c73c5c91b0c257ee45b1658ee9f1
eliran.alon <eliran.alon@avg.com>
2017-05-09 11:54:31 +0300

    Adding privacy policy link in about screen

--- app/src/main/java/com/app/random/backapp/Fragments/AboutFragment.java
+++ app/src/main/java/com/app/random/backapp/Fragments/AboutFragment.java
@@ -27,12 +27,21 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_about, container, false);
         TextView email = (TextView) view.findViewById(R.id.email);
+        TextView privecy = (TextView) view.findViewById(R.id.privacy_policy);
+
         Button done = (Button) view.findViewById(R.id.button);
 
         email.setClickable(true);
+        privecy.setClickable(true);
+
         SpannableString string = new SpannableString("eli200601@gmail.com");
         string.setSpan(new UnderlineSpan(), 0, string.length(), 0);
         email.setText(string);
+
+        SpannableString stringPrivacy = new SpannableString("Privacy Policy");
+        stringPrivacy.setSpan(new UnderlineSpan(), 0, stringPrivacy.length(), 0);
+        privecy.setText(stringPrivacy);
+
         email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +50,17 @@ public class AboutFragment extends Fragment {
                 startActivity(Intent.createChooser(emailIntent, "Send feedback"));
             }
         });
+
+        privecy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://sites.google.com/view/backapp-privacy-policy/home";
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
